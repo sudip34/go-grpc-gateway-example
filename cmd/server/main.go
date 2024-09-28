9c34242d786b8f15b38714ba6013c16f6d9f7d5f
begsaha <begsaha@BEGNBK0215.BEG.NET>
2024-09-28 15:39:50 +0200

    correctd listener in cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,30 +15,30 @@ func main () {
 	const addr = "0.0.0.0:50051"
 
 	// from net create a TCP listner on the specified poet
-	
-	listner, err := net.Listen("tcp", addr)
+
+	listener, err := net.Listen("tcp", addr)
 
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
   // create a gRPC server instance
-	
+
 
 
 	server := grpc.NewServer()
 
 	// create an order service instance with a reference to the DB
-	
+
 	db := internal.NewDB()
 	orderService := internal.NewOrderService(db)
 
 	// register the order service with the grpc server
-	
+
 	orders.RegisterOrdersServer(server, &orderService)
 
 	// start listening to requests
-	
+
 	log.Printf("Server listening at %v", listener.Addr())
 
 	if err = server.Serve(listener); err != nil {
